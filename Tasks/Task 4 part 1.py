cfa4b19bacf38aedd2ff08ddfd90e10b5da75f8a
user <galwa@ac.sce.ac.il>
2024-04-21 21:42:58 +0300

    Gal

--- Tasks/Task 4 part 1.py
+++ Tasks/Task 4 part 1.py
@@ -1,8 +1,53 @@
-import numpy as np
-from matrix_utility import *
+from Quizzes.Quiz01.matrix_utility import *
+
+def lagrange_interpolation(x_data, y_data, x):
+    """
+    Lagrange Interpolation
+
+    Parameters:
+    x_data (list): List of x-values for data points.
+    y_data (list): List of y-values for data points.
+    x (float): The x-value where you want to evaluate the interpolated polynomial.
+
+    Returns:
+    float: The interpolated y-value at the given x.
+    """
+    n = len(x_data)
+    result = 0.0
+
+    for i in range(n):
+        term = y_data[i]
+        for j in range(n):
+            if i != j:
+                term *= (x - x_data[j]) / (x_data[i] - x_data[j])
+        result += term
+
+    return result
 
 
-
+def linearInterpolation(table_points, point):
+    p = []
+    result = 0
+    flag = 1
+    for i in range(len(table_points)):
+        p.append(table_points[i][0])
+    for i in range(len(p) - 1):
+        if i <= point <= i + 1:
+            x1 = table_points[i][0]
+            x2 = table_points[i + 1][0]
+            y1 = table_points[i][1]
+            y2 = table_points[i + 1][1]
+            result = (((y1 - y2) / (x1 - x2)) * point) + ((y2 * x1) - (y1 * x2)) / (x1 - x2)
+            print("\nThe approximation (interpolation) of the point ", point, " is: ", round(result, 4))
+            flag = 0
+    if flag:
+        x1 = table_points[0][0]
+        x2 = table_points[1][0]
+        y1 = table_points[0][1]
+        y2 = table_points[1][1]
+        m = (y1 - y2) / (x1 - x2)
+        result = y1 + m * (point - x1)
+        print("\nThe approximation (extrapolation) of the point ", point, " is: ", round(result, 4))
 
 
 def GaussJordanElimination(matrix, vector):
@@ -64,48 +109,48 @@ def LMatrix(matrix, vector):
 
 def solveMatrix(matrixA, vectorb):
     detA = Determinant(matrixA, 1)
-    print("\nDET(A) = ", detA)
 
     if detA != 0:
-        print("CondA = ", Cond(matrixA, InverseMatrix(matrixA, vectorb)), )
-        print("\nnon-Singular Matrix - Perform GaussJordanElimination", )
         result = GaussJordanElimination(matrixA, vectorb)
-        print(np.array(result))
         return result
     else:
-        print("Singular Matrix - Perform LU Decomposition\n")
-        print("Matrix U: \n")
-        print(np.array(UMatrix(matrixA, vectorb)))
-        print("\nMatrix L: \n")
-        print(np.array(LMatrix(matrixA, vectorb)))
-        print("\nMatrix A=LU: \n")
         result = MultiplyMatrix(LMatrix(matrixA, vectorb), UMatrix(matrixA, vectorb))
-        print(np.array(result))
         return result
 
 
 def polynomialInterpolation(table_points, x):
     matrix = [[point[0] ** i for i in range(len(table_points))] for point in table_points]  # Makes the initial matrix
-
     b = [[point[1]] for point in table_points]
-
-    print("The matrix obtained from the points: ", '\n', np.array(matrix))
-    print("\nb vector: ", '\n', np.array(b))
     matrixSol = solveMatrix(matrix, b)
-
     result = sum([matrixSol[i][0] * (x ** i) for i in range(len(matrixSol))])
-    print("\nThe polynom:", )
-    print('P(X) = ' + '+'.join(['(' + str(matrixSol[i][0]) + ') * x^' + str(i) + ' ' for i in range(len(matrixSol))]))
     print(f"\nThe Result of P(X={x}) is:", )
     print(result)
     return result
 
 
 if __name__ == '__main__':
-    table_points = [(1, 3), (2, 4), (3, -1)]
+    table_points = [(1, 3), (2, 6), (3, 9)]
+    x_data = [1, 2, 3]
+    y_data = [3, 6, 9]
     x = 1.5
-    print("----------------- Interpolation & Extrapolation Methods -----------------\n", )
-    print("Table Points: ", table_points)
-    print("Finding an approximation to the point: ", x, '\n')
-    polynomialInterpolation(table_points, x)
-    print( "\n---------------------------------------------------------------------------\n",)
+    valid = True
+    while (valid):
+        print("You can solve the problem in 3 ways:\n"
+              "1. lagrange interpolation\n"
+              "2. linear Interpolation\n"
+              "3. polynomial Interpolation\n"
+              "What way will you chose?")
+        interpolationMethod = input()
+        valid = False
+        if interpolationMethod == "1":
+            print("By the lagrange interpolation")
+            print("y is: ", lagrange_interpolation(x_data, y_data, x))
+        elif interpolationMethod == "2":
+            print("By the polynomial interpolation")
+            linearInterpolation(table_points, x)
+        elif interpolationMethod == "3":
+            print("By the linear Interpolation")
+            polynomialInterpolation(table_points, x)
+        else:
+            print("incorrect option please try again")
+            valid = True
